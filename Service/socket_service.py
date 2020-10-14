69a3f20e3dd2e4a442e3497253eeabd07d7bc61b
pasibun <matthijs_hovestad@hotmail.com>
2020-10-14 21:02:56 +0200

    connection open houden

--- Service/socket_service.py
+++ Service/socket_service.py
@@ -6,31 +6,30 @@ from Service.control_board_service import ControlBoardService
 
 class Socket(object):
     control_board = ControlBoardService()
-    HOST = '127.0.0.1'
-    PORT = 1337
+    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serv.bind(('0.0.0.0', 1337))
+    serv.listen(5)
+    conn, addr = serv.accept()
     incoming_data = ''
 
     def __init__(self):
-        self.control_board.first_time_run()
-        while True:
-            self.receive_socket()
+        try:
+            self.control_board.first_time_run()
+            while True:
+                self.receive_socket()
+        except KeyboardInterrupt:
+            self.conn.close()
+            print("client disconnected")
 
     def receive_socket(self):
-        serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serv.bind(('0.0.0.0', self.PORT))
-        serv.listen(5)
         while True:
-            conn, addr = serv.accept()
-            while True:
-                data = conn.recv(4096)
-                if not data:
-                    break
-                conn.send(b'Thanks for the message sir')
-                self.incoming_data = data
-            conn.close()
-            print("client disconnected")
-            print("Incoming message: ", self.incoming_data)
-            self.input_socket(self.incoming_data)
+            data = self.conn.recv(4096)
+            if not data:
+                break
+            self.conn.send(b'Thanks for the message sir')
+            self.incoming_data = data
+        print("Incoming message: ", self.incoming_data)
+        self.input_socket(self.incoming_data)
 
     def input_socket(self, input):
         value = ControlButton.FIRST_AXIS_LEFT
